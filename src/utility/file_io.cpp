a0509d3e7cd4973512577bee58bb3d22b54bb8f8
Sophie Starkey <sophiestarkeydev@gmail.com>
2024-06-22 12:51:53 +1200

    Add RAII wrapper for image loading

--- src/utility/file_io.cpp
+++ src/utility/file_io.cpp
@@ -1,7 +1,72 @@
 #include <fstream>
 #include <sstream>
+#include <stb/stb_image.h>
+#include <utility>
 
 #include "file_io.h"
+
+Image::Image(const std::string& path, ImageFormat format)
+{
+    int channels_in_file;
+    m_data = stbi_load(path.c_str(), &m_width, &m_height, &channels_in_file, static_cast<int>(format));
+
+    if (!m_data) {
+        throw std::runtime_error(std::string("Failed to load image '") + path + "': " + stbi_failure_reason());
+    }
+
+    m_num_channels = (format == ImageFormat::USE_DEFAULT) ? channels_in_file : static_cast<int>(format);
+}
+
+Image::~Image()
+{
+    if (m_data) {
+        stbi_image_free(m_data);
+    }
+}
+
+Image::Image(Image&& rhs) noexcept :
+    m_width(std::exchange(rhs.m_width, 0)),
+    m_height(std::exchange(rhs.m_height, 0)),
+    m_num_channels(std::exchange(rhs.m_num_channels, 0)),
+    m_data(std::exchange(rhs.m_data, nullptr))
+{
+}
+
+Image& Image::operator=(Image&& rhs) noexcept
+{
+    if (this != &rhs) {
+        if (m_data) {
+            stbi_image_free(m_data);
+        }
+
+        m_width = std::exchange(rhs.m_width, 0);
+        m_height = std::exchange(rhs.m_height, 0);
+        m_num_channels = std::exchange(rhs.m_num_channels, 0);
+        m_data = std::exchange(rhs.m_data, nullptr);
+    }
+
+    return *this;
+}
+
+int Image::width() const
+{
+    return m_width;
+}
+
+int Image::height() const
+{
+    return m_height;
+}
+
+int Image::num_channels() const
+{
+    return m_num_channels;
+}
+
+stbi_uc* Image::data() const
+{
+    return m_data;
+}
 
 std::string ReadFile(const std::string& path)
 {
--- src/utility/file_io.h
+++ src/utility/file_io.h
@@ -1,5 +1,38 @@
 #pragma once
 
+#include <glm/vec2.hpp>
+#include <stb/stb_image.h>
 #include <string>
+#include <vector>
+
+enum class ImageFormat : int {
+    USE_DEFAULT = 0,
+    GREYSCALE = 1,
+    GREYSCALE_ALPHA = 2,
+    RGB = 3,
+    RGBA = 4
+};
+
+class Image {
+public:
+    Image(const std::string& path, ImageFormat format = ImageFormat::USE_DEFAULT);
+    ~Image();
+
+    Image(const Image&) = delete;
+    Image& operator=(const Image&) = delete;
+
+    Image(Image&& rhs) noexcept;
+    Image& operator=(Image&& rhs) noexcept;
+
+    int width() const;
+    int height() const;
+    int num_channels() const;
+    stbi_uc* data() const;
+private:
+    int m_width;
+    int m_height;
+    int m_num_channels;
+    stbi_uc* m_data;
+};
 
 std::string ReadFile(const std::string& path);
