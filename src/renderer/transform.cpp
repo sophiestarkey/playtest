7e62e4d09b4264df2e71ed46063504aeae35fb2d
Sophie Starkey <sophiestarkeydev@gmail.com>
2024-06-28 11:37:51 +1200

    Add transform class

---
+++ src/renderer/transform.cpp
@@ -0,0 +1,93 @@
+#include "transform.h"
+
+Transform::Transform() :
+m_matrix(1.0f),
+m_recalculate(false),
+m_pivot(0.0f),
+m_position(0.0f),
+m_orientation(1.0f, 0.0f, 0.0f, 0.0f),
+m_scale(1.0f)
+{
+}
+
+void Transform::reset()
+{
+    m_matrix = glm::mat4(1.0f);
+    m_recalculate = false;
+    m_pivot = glm::vec3(0.0f);
+    m_position = glm::vec3(0.0f);
+    m_orientation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+    m_scale = glm::vec3(1.0f);
+}
+
+void Transform::set_pivot(const glm::vec3& pivot)
+{
+    m_pivot = pivot;
+    m_recalculate = true;
+}
+
+void Transform::set_position(const glm::vec3& position)
+{
+    m_position = position;
+    m_recalculate = true;
+}
+
+void Transform::set_orientation(float angle, const glm::vec3& axis)
+{
+    m_orientation = glm::angleAxis(glm::radians(angle), axis);
+    m_recalculate = true;
+}
+
+void Transform::set_scale(const glm::vec3& scale)
+{
+    m_scale = scale;
+    m_recalculate = true;
+}
+
+void Transform::translate(const glm::vec3& offset)
+{
+    m_position += offset;
+    m_recalculate = true;
+}
+
+void Transform::rotate(float angle, const glm::vec3& axis)
+{
+    m_orientation = glm::angleAxis(glm::radians(angle), axis) * m_orientation;
+    m_recalculate = true;
+}
+
+void Transform::scale(const glm::vec3& scale)
+{
+    m_scale *= scale;
+    m_recalculate = true;
+}
+
+const glm::vec3& Transform::right() const
+{
+    return m_orientation * glm::vec3(1.0f, 0.0f, 0.0f);
+}
+
+const glm::vec3& Transform::up() const
+{
+    return m_orientation * glm::vec3(0.0f, 1.0f, 0.0f);
+}
+
+const glm::vec3& Transform::forward() const
+{
+    return m_orientation * glm::vec3(0.0f, 0.0f, 1.0f);
+}
+
+const glm::mat4& Transform::get_matrix() const
+{
+    if (m_recalculate) {
+        m_matrix = glm::mat4(1.0f);
+        m_matrix = glm::translate(m_matrix, m_position);
+        m_matrix = glm::translate(m_matrix, m_pivot);
+        m_matrix *= glm::mat4_cast(m_orientation);
+        m_matrix = glm::translate(m_matrix, -m_pivot);
+        m_matrix = glm::scale(m_matrix, m_scale);
+        m_recalculate = false;
+    }
+
+    return m_matrix;
+}
---
+++ src/renderer/transform.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <glm/gtc/quaternion.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+
+class Transform {
+public:
+    Transform();
+
+    void reset();
+
+    void set_pivot(const glm::vec3& pivot);
+    void set_position(const glm::vec3& position);
+    void set_orientation(float angle, const glm::vec3& axis);
+    void set_scale(const glm::vec3& scale);
+
+    void translate(const glm::vec3& offset);
+    void rotate(float angle, const glm::vec3& axis);
+    void scale(const glm::vec3& scale);
+
+    const glm::vec3& right() const;
+    const glm::vec3& up() const;
+    const glm::vec3& forward() const;
+    const glm::mat4& get_matrix() const;
+private:
+    mutable glm::mat4 m_matrix;
+    mutable bool m_recalculate;
+
+    glm::vec3 m_pivot;
+    glm::vec3 m_position;
+    glm::quat m_orientation;
+    glm::vec3 m_scale;
+};
